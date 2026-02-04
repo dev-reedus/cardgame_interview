b3864e6be2d21c1a321178b80c89399c656cb1be
George Caliment <g.caliment@reply.it>
2026-02-04 14:29:37 +0100

    Updated mocked initial data

--- src/mock-server/initial-data.ts
+++ src/mock-server/initial-data.ts
@@ -9,26 +9,46 @@ export const cards: CardDetail[] = [
     subtitle: "Il Re dello stress 🦆💥",
     card_number: "054",
     health_points: 100,
-    rarity: Rarity.COMMON,
-    image_url: "@/assets/images/source1.png",
+    rarity: Rarity.BASE,
+    image_url: "source1.png",
     short_description:
       "<p>Usare i suoi poteri gli causa mal di testa, per questo passa il tempo cercando di non pensare.</p>",
     long_description:
-      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ol><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ol></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
+      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ul><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ul></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
     typology: {
       name: "acqua",
       icon_url: "",
-      icon_name: "",
+      icon_name: "water",
     },
     energy: {
       name: "stella",
       icon_url: "",
+      icon_name: "star",
     },
     vulnerability: {
       icon_url: "",
       value: -20,
     },
-    extra_details: "",
+    extra_details: {
+      evolutions: [
+        {
+          id: "070dd527-d6d9-48e0-961a-25a40d8bffbf",
+          name: "Golduck",
+          short_description:
+            "<p>Quando nuota a tutta velocità con i suoi lunghi art palmati, la fronte gli si illumina.</p>",
+          image_url: "source7.png",
+        },
+      ],
+      allies: [
+        {
+          id: "070dd527-d6d9-48e0-961b-25a40d8bffbf",
+          name: "Slowpoke",
+          short_description:
+            "<p>Incredibilmente lento e tonto, ci mette cinque secondi a percepire il dolore dopo essere stato colpito.</p>",
+          image_url: "source8.jpg",
+        },
+      ],
+    },
   },
   {
     id: "070dd527-d6d9-48e0-9618-25a40d8bffbg",
@@ -37,26 +57,30 @@ export const cards: CardDetail[] = [
     subtitle: "Il Re dello stress 🦆💥",
     card_number: "054",
     health_points: 100,
-    rarity: Rarity.COMMON,
+    rarity: Rarity.BASE,
     image_url: "@/assets/images/source1.png",
     short_description:
       "<p>Usare i suoi poteri gli causa mal di testa, per questo passa il tempo cercando di non pensare.</p>",
     long_description:
-      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ol><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ol></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
+      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ul><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ul></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
     typology: {
       name: "acqua",
       icon_url: "",
-      icon_name: "",
+      icon_name: "water",
     },
     energy: {
       name: "stella",
       icon_url: "",
+      icon_name: "star",
     },
     vulnerability: {
       icon_url: "",
       value: -20,
     },
-    extra_details: "",
+    extra_details: {
+      evolutions: [],
+      allies: [],
+    },
   },
   {
     id: "070dd527-d6d9-48e0-9618-25a40d8bffbh",
@@ -65,26 +89,30 @@ export const cards: CardDetail[] = [
     subtitle: "Il Re dello stress 🦆💥",
     card_number: "054",
     health_points: 100,
-    rarity: Rarity.COMMON,
+    rarity: Rarity.BASE,
     image_url: "@/assets/images/source1.png",
     short_description:
       "<p>Usare i suoi poteri gli causa mal di testa, per questo passa il tempo cercando di non pensare.</p>",
     long_description:
-      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ol><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ol></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
+      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ul><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ul></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
     typology: {
       name: "acqua",
       icon_url: "",
-      icon_name: "",
+      icon_name: "water",
     },
     energy: {
       name: "stella",
       icon_url: "",
+      icon_name: "star",
     },
     vulnerability: {
       icon_url: "",
       value: -20,
     },
-    extra_details: "",
+    extra_details: {
+      evolutions: [],
+      allies: [],
+    },
   },
   {
     id: "070dd527-d6d9-48e0-9618-25a40d8bffbi",
@@ -93,26 +121,30 @@ export const cards: CardDetail[] = [
     subtitle: "Il Re dello stress 🦆💥",
     card_number: "054",
     health_points: 100,
-    rarity: Rarity.COMMON,
+    rarity: Rarity.BASE,
     image_url: "@/assets/images/source1.png",
     short_description:
       "<p>Usare i suoi poteri gli causa mal di testa, per questo passa il tempo cercando di non pensare.</p>",
     long_description:
-      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ol><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ol></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
+      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ul><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ul></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
     typology: {
       name: "acqua",
       icon_url: "",
-      icon_name: "",
+      icon_name: "water",
     },
     energy: {
       name: "stella",
       icon_url: "",
+      icon_name: "star",
     },
     vulnerability: {
       icon_url: "",
       value: -20,
     },
-    extra_details: "",
+    extra_details: {
+      evolutions: [],
+      allies: [],
+    },
   },
   {
     id: "070dd527-d6d9-48e0-9618-25a40d8bffbl",
@@ -121,26 +153,30 @@ export const cards: CardDetail[] = [
     subtitle: "Il Re dello stress 🦆💥",
     card_number: "054",
     health_points: 100,
-    rarity: Rarity.COMMON,
+    rarity: Rarity.BASE,
     image_url: "@/assets/images/source1.png",
     short_description:
       "<p>Usare i suoi poteri gli causa mal di testa, per questo passa il tempo cercando di non pensare.</p>",
     long_description:
-      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ol><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ol></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
+      '<p>Psyduck vive in uno stato di confusione perenne. Praticamente è il simbolo universale del "non ho capito la domanda". </p><p><ul><li>Il suo superpotere? L\'emicrania. Più gli scoppia la testa, più diventa un dio dei poteri psichici.</li><li>Il dramma: Appena finisce di lanciare attacchi devastanti, si dimentica tutto e torna a fissare il vuoto come se non fosse successo nulla.</li></ul></p><p>In breve: vive con le mani sulle tempie, non sa dove si trova, ma se lo fai arrabbiare ti sposta le montagne con il pensiero. Un eroe moderno.</p>',
     typology: {
       name: "acqua",
       icon_url: "",
-      icon_name: "",
+      icon_name: "water",
     },
     energy: {
       name: "stella",
       icon_url: "",
+      icon_name: "star",
     },
     vulnerability: {
       icon_url: "",
       value: -20,
     },
-    extra_details: "",
+    extra_details: {
+      evolutions: [],
+      allies: [],
+    },
   },
 ];
 
