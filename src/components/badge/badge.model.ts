ba8bc743bffcc157374dd30d81c875bbbad9f6cf
George Caliment <g.caliment@reply.it>
2026-02-04 14:26:17 +0100

    Add badge component

---
+++ src/components/badge/badge.model.ts
@@ -0,0 +1,10 @@
+import type { IconName } from "@/components/icon";
+
+export type BadgeProps = {
+  value?: string | number;
+  icon?: IconName;
+  iconPosition?: "left" | "right";
+  children?: React.ReactNode;
+  className?: string;
+  variant?: "filled" | "translucent";
+};
---
+++ src/components/badge/badge.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import classes from "./badge.module.scss";
+import cn from "classnames";
+import type { BadgeProps } from "@/components/badge/badge.model.ts";
+import { Icon } from "@/components/icon";
+
+const Badge: React.FC<BadgeProps> = ({
+  children,
+  className,
+  value,
+  icon,
+  iconPosition = "left",
+  variant = "filled",
+}) => {
+  const hasValue = value !== undefined;
+
+  const iconNode = icon ? (
+    <span className={classes.icon}>
+      <Icon name={icon} />
+    </span>
+  ) : null;
+
+  return (
+    <span className={cn(classes.badge, className, classes[variant])}>
+      {iconPosition === "left" && iconNode}
+      {hasValue && <span className={classes.value}>{value}</span>}
+      {children}
+      {iconPosition === "right" && iconNode}
+    </span>
+  );
+};
+
+export default Badge;
