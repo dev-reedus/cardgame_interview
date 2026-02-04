cbbf2970bde9ef5558ee2805ed61c4ff8715397d
George Caliment <g.caliment@reply.it>
2026-02-04 14:30:24 +0100

    Updated cards routes

--- src/views/cards/cards.tsx
+++ src/views/cards/cards.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import CardListPage from "./card-list/card-list.tsx";
+import CardListPage from "./card-list";
+import CardDetail from "./card-detail";
 
 const Cards: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<CardListPage />} />
-      <Route path="/:id" element={<h1>404 - Page Not Found</h1>} />
+      <Route path="/:id" element={<CardDetail />} />
     </Routes>
   );
 };
