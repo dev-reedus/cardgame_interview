ea31acb529aa77d81115f3d65c8e3cf5c311615f
George Caliment <g.caliment@reply.it>
2026-02-05 22:08:19 +0100

    Fixed styles loading on storybook preview

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,6 @@
 import type { Preview } from "@storybook/react-vite";
 import { initialize, mswLoader } from "msw-storybook-addon";
+import "../src/assets/styles/global.scss";
 
 initialize({
   onUnhandledRequest: "bypass",
